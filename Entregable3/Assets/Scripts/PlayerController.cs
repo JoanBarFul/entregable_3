a126b234bca4e06ef6cf65c56c3727ddc5d8957b
JoanBarFul <91464434+JoanBarFul@users.noreply.github.com>
2021-10-26 20:45:26 +0200

    3

--- Entregable3/Assets/Scripts/PlayerController.cs
+++ Entregable3/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float scale = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3.(0, 0, 0);
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        { transform.localScale -= Vector3.right; }
+        if (Input.GetKeyDown(KeyCode.X))
+        { transform.localScale -= new Vector3(scale, 0, 0); }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        { transform.localScale -= new Vector3(0, 0, scale); }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        { transform.localScale -= new Vector3(0, scale, 0); }
     }
 }
